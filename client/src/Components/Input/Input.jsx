66b75e8e33e4336272dcb4b9ca5f5a91c698e813
kopildas <102140363+kopildas@users.noreply.github.com>
2024-01-02 21:43:08 +0600

    fix: multiple data added function

--- client/src/Components/Input/Input.jsx
+++ client/src/Components/Input/Input.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
-export default function Input({name,onChange,value}) {
+export default function Input({name,onChange,value,label}) {
   const onChangeFunc = (e)=> {
       onChange(e);
     }
   return (
-    <div className="relative float-label-input h-8">
+    <div className="relative bg-zinc-800 float-label-input h-8">
                   <input
                     required
                     type="text"
                     id={name}
                     placeholder=" "
-                    className="block bg-zinc-800  focus:outline-none focus:shadow-outline border border-gray-300 rounded-md py-2 px-3  appearance-none leading-normal focus:border-blue-400"
-                    // value={name}
+                    className="block bg-zinc-800  focus:outline-none focus:shadow-outline border border-gray-300 rounded-md py-2 px-3  appearance-none leading-normal focus:border-blue-400 text-gray-100"
+                    value={value}
                     onChange={onChangeFunc}
                   />
                   <label className="absolute top-3 bg-zinc-800 text-sm left-0 text-gray-400 pointer-events-none transition duration-200 ease-in-out  px-2 text-grey-darker">
-                    {value}
+                    {label}
                   </label>
                 </div>
   )
